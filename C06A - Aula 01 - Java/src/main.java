cf41c666d322b1654ebf97bb1f0d17e59951c5c8
RichardAlvesMatos <richardalvesmatos@gmail.com>
2026-02-10 14:34:25 -0300

    Add files via upload

---
+++ C06A - Aula 01 - Java/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println("O valor de z é: " + z);
+
+        System.out.println("Hello world!");
+
+    }
+
+}
