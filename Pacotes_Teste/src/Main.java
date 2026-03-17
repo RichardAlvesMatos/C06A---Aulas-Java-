156a05e544918675def8bcdbbd8b776aa31da263
RichardAlvesMatos <richardalvesmatos@gmail.com>
2026-03-17 14:26:05 -0300

    Add files via upload

---
+++ Pacotes_Teste/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Matematica ma = new Matematica();
+
+        ma.numero = 1000;
+        ma.numero1 = numero;
+
+        ma.setNumero(1000);
+    }
+}
---
+++ Pacotes_Teste/src/br/inatel/soma/Matematica.java
@@ -0,0 +1,29 @@
+package br.inatel.soma;
+
+public class Matematica {
+
+    private int numero;
+    private int numero1;
+
+
+    public int getNumero(){
+
+        return numero;
+    }
+
+    public void setNumero(){
+
+        this.numero = numero;
+    }
+
+    public void manipulaNumero(){
+
+        numero = 1000;
+        numero = numero1;
+    }
+
+    public void rafaelYohanam(){
+
+
+    }
+}
